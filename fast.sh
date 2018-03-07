1261fcc89c092e9428ce447e25fe8ef6dd93efa6
vattay <3martini@gmail.com>
2018-03-06 19:49:21 -0500

    Add initial stuff.

---
+++ fast.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+devices=( $(nvidia-smi -L | awk '{print $2}' | sed 's/.$//' | tr '\n' ' ') )
+echo "Detected GPU devices $devices"
+
+clock=110
+mem=300
+echo "Setting core to $clock and mem to $mem"
+
+for id in "${devices[@]}"
+do
+    nvidia-settings -a "[gpu:$id]/GPUFanControlState=1"
+    nvidia-settings -a "[fan:$id]/GPUTargetFanSpeed=88"
+    nvidia-settings -a "[gpu:$id]/GPUGraphicsClockOffset[3]=$clock"
+    nvidia-settings -a "[gpu:$id]/GPUMemoryTransferRateOffset[3]=$mem"
+done
+
+wait
---
+++ fix-py-support.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget http://launchpadlibrarian.net/109052632/python-support_1.0.15_all.deb
+sudo dpkg -i python-support_1.0.15_all.deb
