0b9438565fbb70dede2072f98c4753bacacae2fd
vattay <3martini@gmail.com>
2018-05-31 21:32:48 -0400

    Removed uneeded force on apt.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt update && \
-sudo apt install -f -y ocl-icd-opencl-dev && \
+sudo apt install -y ocl-icd-opencl-dev && \
 sudo apt install -f -y ./debs/fahclient_7.5.1_amd64.deb && \
 sudo apt install -f -y ./debs/fahcontrol_7.5.1-1_all.deb && \
 sudo apt install -f -y ./debs/fahviewer_7.5.1_amd64.deb
