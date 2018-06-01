9ebd9f8681bed99dd46807c2bd8951b69caea6ce
vattay <3martini@gmail.com>
2018-05-31 22:04:22 -0400

    Support usage where DISPLAY not defined

--- fast.sh
+++ fast.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+NDISP=''
+
+if [ -z ${DISPLAY+x} ]; then
+    NDISP=':0'
+else 
+    NDISP=$DISPLAY
+fi
+
+echo "Using DISPLAY $DISPLAY"
+
 devices=( $(nvidia-smi -L | awk '{print $2}' | sed 's/.$//' | tr '\n' ' ') )
 echo "Detected GPU devices $devices"
 
@@ -9,10 +19,10 @@ echo "Setting core to $clock and mem to $mem"
 
 for id in "${devices[@]}"
 do
-    nvidia-settings -a "[gpu:$id]/GPUFanControlState=1"
-    nvidia-settings -a "[fan:$id]/GPUTargetFanSpeed=88"
-    nvidia-settings -a "[gpu:$id]/GPUGraphicsClockOffset[3]=$clock"
-    nvidia-settings -a "[gpu:$id]/GPUMemoryTransferRateOffset[3]=$mem"
+    nvidia-settings -c $NDISP -a "[gpu:$id]/GPUFanControlState=1"
+    nvidia-settings -c $NDISP -a "[fan:$id]/GPUTargetFanSpeed=100"
+    nvidia-settings -c $NDISP -a "[gpu:$id]/GPUGraphicsClockOffset[3]=$clock"
+    nvidia-settings -c $NDISP -a "[gpu:$id]/GPUMemoryTransferRateOffset[3]=$mem"
 done
 
 wait
